321619db8e1fb5557e1fc501c1e7175ccb8b6bc7
Patrick Poirier <patrick.poirier2@mail.mcgill.ca>
2016-07-07 23:02:06 -0400

    fixed config

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pusher_app_id: "222770",
+  pusher_key: "25d18135c241f60bfb1f",
+  pusher_secret: "e4b58f1a717b7fe628d4",
+  twitter_consumer_key: "fXQpQZErDubrT5TzPMlGzQKwn",
+  twitter_consumer_secret: "zbHY57k1Rd2yI55WR6mDgZT0k8u4nGcwIxecOtRsixta83VbGi",
+  twitter_access_token_key: "1137419018-eLPjPBNXjCBxB3u3ao5S49gEYPn5BlI6Zpu4XPZ",
+  twitter_access_token_secret: "rhEXrWzFAkwRyHzkAE6zFUIdmOZz3yv5dfVDdfq8h5bdh",
+  keywords: ["Erudite Science", "EdTech", "Obama"]
+}
