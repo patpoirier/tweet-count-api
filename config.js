fd0477d04df49e73ab7f16cbe19d3b762fa472b2
jpbeaudet <jpbeaudet84@gmail.com>
2016-07-22 12:13:33 -0400

    modified config

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
   pusher_app_id: "222770",
   pusher_key: "25d18135c241f60bfb1f",
   pusher_secret: "e4b58f1a717b7fe628d4",
-  twitter_consumer_key: "fXQpQZErDubrT5TzPMlGzQKwn",
-  twitter_consumer_secret: "zbHY57k1Rd2yI55WR6mDgZT0k8u4nGcwIxecOtRsixta83VbGi",
+  twitter_consumer_key: "hqfTEgWc8Yue4lgji2HCII0tl",
+  twitter_consumer_secret: "ok5610mhnaueDXSIVCBOuVx16n62MUp8Z1ssGxbCKJBT4UCNOg",
   twitter_access_token_key: "1137419018-eLPjPBNXjCBxB3u3ao5S49gEYPn5BlI6Zpu4XPZ",
   twitter_access_token_secret: "rhEXrWzFAkwRyHzkAE6zFUIdmOZz3yv5dfVDdfq8h5bdh",
   keywords: []
