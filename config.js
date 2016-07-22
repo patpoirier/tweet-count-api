f29198189fd65ab339ad83105971716f4b3da9df
jpbeaudet <jpbeaudet84@gmail.com>
2016-07-21 22:19:26 -0400

    removing hard coded keywords

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ module.exports = {
   twitter_consumer_secret: "zbHY57k1Rd2yI55WR6mDgZT0k8u4nGcwIxecOtRsixta83VbGi",
   twitter_access_token_key: "1137419018-eLPjPBNXjCBxB3u3ao5S49gEYPn5BlI6Zpu4XPZ",
   twitter_access_token_secret: "rhEXrWzFAkwRyHzkAE6zFUIdmOZz3yv5dfVDdfq8h5bdh",
-  keywords: ["Erudite Science", "EdTech", "Obama"]
+  keywords: []
 }
